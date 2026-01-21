1b0be4006d12ca9b9073bda5926e9807643374a4
jnu_server <suseong0401@gmail.com>
2026-01-21 14:26:00 -0500

    feat: 중복된 부분 제거 및 essid 구하기 구현

--- beacon.cpp
+++ beacon.cpp
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <string>
 #include <pcap.h>
 #include "beacon.h"
 
-// http://www.ktword.co.kr/test/view/view.php?no=2319
-ST_BC_HDR parseBeacon(const u_char* packet)
+//http://www.ktword.co.kr/test/view/view.php?m_temp1=4906&id=758
+ST_BC parseBeacon(const u_char* packet)
 {
-    ST_BC_HDR *pHdr = (ST_BC_HDR*)packet; 
+    ST_BC *pHdr = (ST_BC*)packet;
     return *pHdr;
 }
 
+std::string getEssid(const u_char* packet, const int beaconLen)
+{
+    const u_char* index = packet;
+    const u_char* end = index + beaconLen;
+    while(index < end)
+    {
+        uint8_t tagId = index[0];
+        uint8_t tagLen = index[1];
+        const u_char* data = index + 2;
+        if (tagId == 0)
+        {   
+            if (tagLen == 0) return "<length: 0>";
+            // <length: n> 찍히던거는 00으로 설정된거 길이인듯..?
+            if (data[0] == 0 | data[1] == 0) return "<length: " + std::to_string(tagLen) + ">";
+            return std::string((char*)data, tagLen);
+        }
+        index += (2 + tagLen);
+    }
+    
+    return ""; 
+}
--- beacon.h
+++ beacon.h
@@ -1,26 +1,9 @@
-
-struct ST_BC_HDR
-{
-    uint16_t framControl;
-    uint16_t duration_id;
-    uint8_t da[6];
-    uint8_t sa[6];
-    uint8_t bssid[6];
-    uint16_t seqControl;
-} __attribute__((packed));
-
-struct ST_BC_BODY_ESSENTIAL
+struct ST_BC
 {
     uint64_t timestamp;
     uint16_t interval;
-    uint16_t capability;
-    uint8_t elementId;
-    uint8_t len;
-    uint8_t ssid[];
+    uint16_t capacity;
 } __attribute__((packed));
 
-struct BC_BODY_TAG
-{
-} __attribute__((packed));
-
-ST_BC_HDR parseBeacon(const u_char* packet);
+ST_BC parseBeacon(const u_char* packet);
+std::string getEssid(const u_char* packet, const int beaconLen);
